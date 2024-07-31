410a035e3aa32cfa939c2f01b52d4b39bb03fbef
basanez86 <basanez86@gmail.com>
2024-07-31 10:53:10 -0700

    Release version 2.0.8

--- start.bat
+++ start.bat
@@ -8,6 +8,7 @@ REM Set the path to the env python executable
 set ENV_PYTHON=%SCRIPT_DIR%env\python.exe
 
 REM Install requirements if needed
+%ENV_PYTHON% -m pip install --upgrade pip
 %ENV_PYTHON% -m pip install -r %SCRIPT_DIR%requirements.txt
 
 REM Run the application
