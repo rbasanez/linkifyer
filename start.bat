ee57475e5b5632ec90412beb2e08f3b3d10bfd3b
basanez86 <basanez86@gmail.com>
2024-07-31 10:41:24 -0700

    Release version 2.0.7

--- start.bat
+++ start.bat
@@ -1,3 +1,16 @@
 @echo off
+setlocal
 
-%~dp0env\python.exe %~dp0app.py
+REM Get the directory of the current script
+set SCRIPT_DIR=%~dp0
+
+REM Set the path to the env python executable
+set ENV_PYTHON=%SCRIPT_DIR%env\python.exe
+
+REM Install requirements if needed
+%ENV_PYTHON% -m pip install -r %SCRIPT_DIR%requirements.txt
+
+REM Run the application
+%ENV_PYTHON% %SCRIPT_DIR%app.py
+
+endlocal
